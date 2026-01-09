d74540a931797c255849412d531abceac5167b0a
Baptiste JULLION <contact@baptiste-jullion.dev>
2026-01-09 10:22:23 +0100

    add unit tests for UserService

---
+++ back/src/test/java/com/openclassrooms/starterjwt/services/UserServiceTest.java
@@ -0,0 +1,60 @@
+package com.openclassrooms.starterjwt.services;
+
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("UserService")
+public class UserServiceTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    private User user;
+
+    @BeforeEach
+    void setup() {
+        user = User.builder()
+                   .id(1L)
+                   .firstName("John")
+                   .lastName("Doe")
+                   .email("user@example.com")
+                   .password("password")
+                   .admin(false)
+                   .build();
+    }
+
+    @Test
+    @DisplayName("Find a user by ID")
+    void findByIdReturnsUser() {
+        when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+
+        User foundUser = userService.findById(user.getId());
+
+        assertThat(foundUser).usingRecursiveComparison()
+                             .isEqualTo(user);
+        verify(userRepository, times(1)).findById(user.getId());
+    }
+
+    @Test
+    @DisplayName("Delete a user by ID")
+    void deleteUserById() {
+        userService.delete(user.getId());
+
+        verify(userRepository, times(1)).deleteById(user.getId());
+    }
+}
